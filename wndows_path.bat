9ac6e48e3336a8e4e258fc43aa2e774341cf8262
eirblaze <eirblaze@live.jp>
2019-07-19 21:59:23 +0900

    powershellに変換

--- wndows_path.bat
+++ wndows_path.bat
@@ -1,2 +1,4 @@
+@powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
+
 setx PATH2 "%PATH2%;%~d1%~p1\"
 pause

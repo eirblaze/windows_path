79ce7b5c58a3da2e2c0e725bf5214135e37b538b
eirblaze <eirblaze@live.jp>
2019-07-19 22:48:53 +0900

    実験を本番へ

--- jikken.bat
+++ jikken.bat
@@ -1,6 +1,8 @@
 @echo off
+
 set newpath=%~d1%~p1
 set newpath=%PATH2%;%newpath:~0,-1%
-set newpath="%newpath:~0,-1%"
+set newpath="%newpath%"
+
 echo %newpath%
 pause
--- wndows_path.bat
+++ wndows_path.bat
@@ -1,3 +1,8 @@
-set newpath=;%~d1%~p1
-setx PATH2 "%PATH2%;%newpath:~0,-1%\"
+@echo off
+
+set newpath=%~d1%~p1
+set newpath=%PATH2%;%newpath:~0,-1%
+set newpath="%newpath%"
+
+setx PATH2 %newpath%
 pause

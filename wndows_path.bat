5e8998ea1511223fafbc05e5d0b44969cb870e62
eirblaze <eirblaze@live.jp>
2019-07-19 22:00:40 +0900

    powershellの書き方に変更

--- wndows_path.bat
+++ wndows_path.bat
@@ -1,4 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
 
-setx PATH2 "%PATH2%;%~d1%~p1\"
-pause
+$env:Path += ";C:\Program Files\OpenSSH"
+[Environment]::SetEnvironmentVariable('PATH', $Env:Path, 'Machine')
+

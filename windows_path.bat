71c39dff57caccd66f4519b97ba0f0fd42ff6b1e
eirblaze <eirblaze@live.jp>
2019-11-10 11:31:38 +0900

    デバッグメッセージにする

--- windows_path.bat
+++ windows_path.bat
@@ -2,13 +2,13 @@
 
 # 引数
 Param($Arg1)
-Write-Host $Arg1
+Write-Debug $Arg1
 
 # ファイル存在チェック
-Write-Host "ファイル存在チェック"
+Write-Debug "ファイル存在チェック"
 
 if( -Not(Test-Path $Arg1) ){
-    Write-Host '指定されたファイルは存在しません。処理を終了します。'
+    Write-Debug '指定されたファイルは存在しません。処理を終了します。'
     exit
 }
 
@@ -16,16 +16,16 @@ if( -Not(Test-Path $Arg1) ){
 $item = Get-Item $Arg1
 
 # PSIsContainer でファイルかフォルダかを判定 https://bayashita.com/p/entry/show/229
-Write-Host "ファイルかフォルダかを判定"
+Write-Debug "ファイルかフォルダかを判定"
 if($item.PSIsContainer)
 {
     # フォルダの場合の処理
-    Write-Host "$($item.Name)はフォルダです。"
+    Write-Debug "$($item.Name)はフォルダです。"
 }
 else
 {
     # ファイルの場合は・・・
-    Write-Host "$($item.Name)はファイルです。"
+    Write-Debug "$($item.Name)はファイルです。"
 
     # 親フォルダをアイテムとして再取得
     # バヤシタ ＞ PowerShell ＞ ファイル・フォルダ ＞ 親フォルダの絶対パスを取得する方法 https://bayashita.com/p/entry/show/133
@@ -35,31 +35,31 @@ else
     $item = Get-Item $parent_folder
 
     if ($item.PSIsContainer) {
-        Write-Host "アイテムを$($item.Name)として再取得成功"
+        Write-Debug "アイテムを$($item.Name)として再取得成功"
     }
 }
-Write-Host "フルネーム $($item.FullName)"
+Write-Debug "フルネーム $($item.FullName)"
 
 # 新しい環境変数を生成。新規／上書きで処理を分岐。
 
 # 新規の場合
 if ( -Not(Test-Path Env:Path2) ) {
     # 新しい文字列を生成
-    Write-Host "new Path2"
+    Write-Debug "new Path2"
     $NewPath = $item.FullName
 }
 # 追加の場合
 else
 {
     # 新しい文字列を生成
-    Write-Host "add Path2"
+    Write-Debug "add Path2"
     $NewPath = "$($env:Path2);$($item.FullName)"
 }
 
 # 環境変数をセット
-Write-Host "セット予定 $($NewPath)"
+Write-Debug "セット予定 $($NewPath)"
 [Environment]::SetEnvironmentVariable('Path2', $NewPath, 'User')
 
-Write-Host "現在のenv:Path2 $($env:Path2)"
+Write-Debug "現在のenv:Path2 $($env:Path2)"
 
-Write-Host "---------------------------------------------------------------------------------------"
+Write-Debug "---------------------------------------------------------------------------------------"

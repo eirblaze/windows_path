02dcb33a15f04373be409d52ed99d87f11b3ae59
eirblaze <eirblaze@live.jp>
2020-02-09 20:51:34 +0900

    Verboseに置き換え

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-REM .\test\jikken.bat "D:\test\test dir\notfound.bat"
-.\test\jikken.bat "D:\test\test dir\test file.bat"
-.\test\jikken.bat "D:\test\test dir"
+REM .\test\jikken.ps1.bat "D:\test\test dir\notfound.bat"
+.\test\jikken.ps1.bat "D:\test\test dir\test file.bat"
+.\test\jikken.ps1.bat "D:\test\test dir"
 pause
